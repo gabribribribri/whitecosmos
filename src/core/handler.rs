cb0ccc5365a84bc1074e9bc8f812c05b4caae79a
gabribribribri <fuelanemo149@gmail.com>
2026-07-08 19:53:13 +0200

    implemented label hunt. hopefully it works. we will test later

--- src/core/handler.rs
+++ src/core/handler.rs
@@ -1,7 +1,12 @@
 use std::collections::HashMap;
 
 use crate::{
-    backend::runtime::{Runtime, RuntimeError, RuntimeErrorFlowCtrl, RuntimeReport}, core::{handler_errors::EngineError, statements::Statement}, frontend::parser::{ParseError, ParseResult, Parser}
+    backend::runtime::{Runtime, RuntimeError, RuntimeErrorFlowCtrl, RuntimeReport},
+    core::{
+        handler_errors::EngineError,
+        statements::{Statement, StatementFlowCtrl},
+    },
+    frontend::parser::{ParseError, ParseResult, Parser},
 };
 
 pub struct Handler {
@@ -32,16 +37,19 @@ impl Handler {
 
             use RuntimeReport::*;
             match action {
-                Next => stat_index += 1,
+                Next => (),
                 EndProgram => return Ok(()),
                 MarkLabel(label) => _ = self.labels.insert(label, stat_index),
                 JumpTo(label) => match self.labels.get(&label) {
                     Some(location) => stat_index = *location,
-                    None => {
-                        return Err(EngineError::Runtime(RuntimeError::FlowCtrl(
-                            RuntimeErrorFlowCtrl::LabelNotFound,
-                        )));
-                    }
+                    None => match self.hunt_label(label)? {
+                        Some(location) => stat_index = location,
+                        None => {
+                            return Err(EngineError::Runtime(RuntimeError::FlowCtrl(
+                                RuntimeErrorFlowCtrl::LabelNotFound,
+                            )));
+                        }
+                    },
                 },
                 CallSubroutine(label) => match self.labels.get(&label) {
                     Some(location) => {
@@ -63,15 +71,25 @@ impl Handler {
                     }
                 },
             }
+            stat_index += 1;
         }
     }
 
-    fn hunt_labels(&mut self, label_to_find: i32) -> Result<Option<i32>, ParseError> {
-        let mut running_index = self.statements.len()-1;
-        loop {
-            running_index += 1;
-
+    fn hunt_label(&mut self, label_to_find: i32) -> Result<Option<usize>, ParseError> {
+        for running_index in self.statements.len().. {
+            use {Statement::*, StatementFlowCtrl::*};
+            match self.read_statement(running_index)? {
+                FlowCtrl(MarkLabel(label)) => {
+                    self.labels.insert(label, running_index);
+                    if label == label_to_find {
+                        return Ok(Some(running_index));
+                    }
+                }
+                FlowCtrl(EndProgram) => return Ok(None),
+                _ => (),
+            }
         }
+        todo!() // TODO find a way to exit properly
     }
 
     fn read_statement(&mut self, stat_index: usize) -> ParseResult {
